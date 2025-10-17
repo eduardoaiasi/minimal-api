9f1d5c35fd3cc1ab5f242b432be4fae998c09b36
eduardo <eduardo.diniz.aiasi@gmail.com>
2025-10-17 20:47:05 -0300

    Implement login functionality with POST route and DTO for user credentials

--- Program.cs
+++ Program.cs
@@ -1,7 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+// Rota GET simples
 app.MapGet("/", () => "Hello World!");
-app.MapGet("/login", () => "Login Page");
+
+// Rota POST que recebe um JSON e faz uma verificação
+app.MapPost("/login", (LoginDTO loginDTO) =>
+{
+    if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+    {
+        return Results.Ok("Login successful");
+    }
+    else
+    {
+        return Results.Unauthorized();
+    }
+});
 
 app.Run();
+
+// DTO usado no POST
+public class LoginDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+}
--- obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+999013cf1da59e0773128f3db1abecce613747e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
