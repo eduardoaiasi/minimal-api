6afda72f321ec83c4ade8e8586af408d41393e13
eduardo <eduardo.diniz.aiasi@gmail.com>
2025-10-17 21:17:41 -0300

    Update AssemblyInfo and add .gitignore for project configuration

--- obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
+++ obj/Debug/net9.0/minimal-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f1d5c35fd3cc1ab5f242b432be4fae998c09b36")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8655684efa7c42281cdd4dbe135cd8adde5041ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
